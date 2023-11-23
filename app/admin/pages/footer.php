44e1d4a76f7657ad8d40aa9b353993ce3fa8e20f
fahmiekoputrosantoso <fahmieko73@gmail.com>
2023-11-23 12:02:47 +0700

    test

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?></strong> Fahmi Eko Putro Santoso <a href='https://www.instagram.com/miikoo.id/' title='https://www.instagram.com/' target='_blank'>Instagram</a>
+    
+</footer>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+//------------------------------::::::::::::::::::::------------------------------\\
+// Dibuat oleh Fahmi Eko Putro Santoso || 312010046 \\
+//------------------------------::::::::::::::::::::------------------------------\\
+
+// Arahkan user ketika pertama kali membuka applikasi ke halaman login
+header("location: masuk");
